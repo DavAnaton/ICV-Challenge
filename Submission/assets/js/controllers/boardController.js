ac9f9b6e1cf632e34e041ade062c00ba41120c03
DavAna <david@anaton.fr>
2017-08-07 20:19:19 +0300

    Section 2
    Selecting / Deselecting tiles

--- Submission/assets/js/controllers/boardController.js
+++ Submission/assets/js/controllers/boardController.js
@@ -3,4 +3,17 @@ angular.module("PathFinder")
 
 function BoardController($scope){
     $scope.size = 12;
+    $scope.tiles = [];
+
+    $scope.resetTiles = function(){
+        $scope.tiles = [];
+        for (i = 0; i < $scope.size*$scope.size; i++) {
+            $scope.tiles.push({selected: false});
+        }
+    }
+    $scope.resetTiles();
+
+    $scope.tileClicked = function(index){
+        $scope.tiles[index].selected = !$scope.tiles[index].selected; 
+    }
 };
